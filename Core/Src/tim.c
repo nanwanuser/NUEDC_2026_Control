de7cac2d69001a6e8b5cee9b1a7a7751fa42b6b5
lirui liu <3104084184@qq.com>
2026-07-18 16:40:16 +0800

    fix: move servo PWM off encoder pin
    Use TIM1 CH1 and CH2 for the PE9/PE11 encoder inputs and TIM1 CH3 on PE13 for the servo PWM output. Add encoder counter wrap handling for the shared 20 ms timer period and update CubeMX pin/channel assignments.

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -35,6 +35,7 @@ void MX_TIM1_Init(void)
   /* USER CODE END TIM1_Init 0 */
 
   TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_Encoder_InitTypeDef sConfigEncoder = {0};
   TIM_OC_InitTypeDef sConfigOC = {0};
   TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
 
@@ -48,7 +49,16 @@ void MX_TIM1_Init(void)
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
-  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
+  sConfigEncoder.EncoderMode = TIM_ENCODERMODE_TI12;
+  sConfigEncoder.IC1Polarity = TIM_ICPOLARITY_RISING;
+  sConfigEncoder.IC1Selection = TIM_ICSELECTION_DIRECTTI;
+  sConfigEncoder.IC1Prescaler = TIM_ICPSC_DIV1;
+  sConfigEncoder.IC1Filter = 0;
+  sConfigEncoder.IC2Polarity = TIM_ICPOLARITY_RISING;
+  sConfigEncoder.IC2Selection = TIM_ICSELECTION_DIRECTTI;
+  sConfigEncoder.IC2Prescaler = TIM_ICPSC_DIV1;
+  sConfigEncoder.IC2Filter = 0;
+  if (HAL_TIM_Encoder_Init(&htim1, &sConfigEncoder) != HAL_OK)
   {
     Error_Handler();
   }
@@ -65,7 +75,7 @@ void MX_TIM1_Init(void)
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
   sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
-  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
   {
     Error_Handler();
   }
@@ -87,16 +97,25 @@ void MX_TIM1_Init(void)
 
 }
 
-void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef* tim_pwmHandle)
+void HAL_TIM_Encoder_MspInit(TIM_HandleTypeDef* tim_encoderHandle)
 {
 
-  if(tim_pwmHandle->Instance==TIM1)
+  if(tim_encoderHandle->Instance==TIM1)
   {
   /* USER CODE BEGIN TIM1_MspInit 0 */
 
   /* USER CODE END TIM1_MspInit 0 */
     /* TIM1 clock enable */
     __HAL_RCC_TIM1_CLK_ENABLE();
+    __HAL_RCC_GPIOE_CLK_ENABLE();
+
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    GPIO_InitStruct.Pin = GPIO_PIN_9 | GPIO_PIN_11;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF1_TIM1;
+    HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
   /* USER CODE BEGIN TIM1_MspInit 1 */
 
   /* USER CODE END TIM1_MspInit 1 */
@@ -114,9 +133,9 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* timHandle)
 
     __HAL_RCC_GPIOE_CLK_ENABLE();
     /**TIM1 GPIO Configuration
-    PE9     ------> TIM1_CH1
+    PE13     ------> TIM1_CH3 (servo PWM)
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_9;
+    GPIO_InitStruct.Pin = GPIO_PIN_13;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -130,16 +149,17 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* timHandle)
 
 }
 
-void HAL_TIM_PWM_MspDeInit(TIM_HandleTypeDef* tim_pwmHandle)
+void HAL_TIM_Encoder_MspDeInit(TIM_HandleTypeDef* tim_encoderHandle)
 {
 
-  if(tim_pwmHandle->Instance==TIM1)
+  if(tim_encoderHandle->Instance==TIM1)
   {
   /* USER CODE BEGIN TIM1_MspDeInit 0 */
 
   /* USER CODE END TIM1_MspDeInit 0 */
     /* Peripheral clock disable */
     __HAL_RCC_TIM1_CLK_DISABLE();
+    HAL_GPIO_DeInit(GPIOE, GPIO_PIN_9 | GPIO_PIN_11 | GPIO_PIN_13);
   /* USER CODE BEGIN TIM1_MspDeInit 1 */
 
   /* USER CODE END TIM1_MspDeInit 1 */
