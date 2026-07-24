032094687d31b1e02115ed103ac4adf6a83598b5
tsyw1 <3510574746@qq.com>
2026-07-24 20:17:59 +0800

    提交simulink_pid

---
+++ App/lib/simulink_pid/simulink_pid_ert_rtw/setup_msvc.bat
@@ -0,0 +1,2 @@
+set "VSCMD_START_DIR=%CD%"
+CALL "D:\visual_studio\VC\Auxiliary\Build\VCVARSALL.BAT " amd64
---
+++ App/lib/simulink_pid/simulink_pid_ert_rtw/simulink_pid_types.h
@@ -0,0 +1,28 @@
+/*
+ * File: simulink_pid_types.h
+ *
+ * Code generated for Simulink model 'simulink_pid'.
+ *
+ * Model version                  : 1.6
+ * Simulink Coder version         : 24.1 (R2024a) 19-Nov-2023
+ * C/C++ source code generated on : Fri Jul 24 18:14:44 2026
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef simulink_pid_types_h_
+#define simulink_pid_types_h_
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_simulink_pid_T RT_MODEL_simulink_pid_T;
+
+#endif                                 /* simulink_pid_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
